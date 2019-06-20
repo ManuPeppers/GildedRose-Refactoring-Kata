d6090df552b5d59df12f53722fe4d4efc8faf5b7
ManuPeppers <manuel.grimaldi@xpeppers.com>
2019-06-20 17:33:16 +0200

    test + classe AttributeCalculator" git commit

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,92 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void decreaseSellinAndQUality() {
+        Item[] items = new Item[] { new Item("Item1", 10, 10) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(new Item("Item1",9,9).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityAgedBrie(){
+        Item[] items = new Item[]{new Item("Aged Brie",2,0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Aged Brie",1,1).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualitySulfuras(){
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros",2,80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Sulfuras, Hand of Ragnaros",2,80).toString(), items[0].toString());
+    }
+
+
+    @Test
+    public void decreaseSellinAndQualityBackstageByTwo(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",10,20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Backstage passes to a TAFKAL80ETC concert",9,22).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityBackstageByThree(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",5,20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Backstage passes to a TAFKAL80ETC concert",4,23).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityBackstage(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",15,20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Backstage passes to a TAFKAL80ETC concert",14,21).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityNotMoreThanFifty(){
+        Item[] items = new Item[]{new Item("Aged Brie",13,50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Aged Brie",12,50).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityBSampleItemQualityNotNegative(){
+        Item[] items = new Item[]{new Item("Sample StandardItem",4,0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Sample StandardItem",3,0).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityExpiredSellinDate(){
+        Item[] items = new Item[]{new Item("Sample StandardItem",0,20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Sample StandardItem",-1,18).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityExpiredSellinDateQualityNotMinusZero(){
+        Item[] items = new Item[]{new Item("Aged Brie",-1,47)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Aged Brie",-2,49).toString(), items[0].toString());
+    }
+
+    @Test
+    public void decreaseSellinAndQualityBackStageExpiredSellinDateQuality(){
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert",-1,47)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(new Item("Backstage passes to a TAFKAL80ETC concert",-2,0).toString(), items[0].toString());
     }
 
 }
